a1c7939d83ff95623cc21bfa289dddb6bbddfe12
Alexandr Golubev <deed27@narod.ru>
2016-11-25 18:28:07 +0400

    add structure

---
+++ chapter_001/src/test/java/ru/alexandr/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.alexandr;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*Test.
+*
+*@autor Alexandr Golubev (deed27@narod.ru)
+*@version $Id$
+*since 0.1
+*/
+public class CalculateTest {
+	/**
+	* Test add.
+	*/
+    @Test
+    public void whenExecuteMainThenPrintToConsole() {
+	    ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello world!\r\n"));
+    }
+}
