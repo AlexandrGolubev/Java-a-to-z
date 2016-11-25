cc267d1a537c603e1060fe55e834f33b0d1c6e60
Alexandr Golubev <deed27@narod.ru>
2016-11-25 07:34:02 +0400

    add Calculate.java

--- chapter_001/src/main/java/ru/alexandr/Calculate.java
+++ chapter_001/src/main/java/ru/alexandr/Calculate.java
@@ -1,6 +1,18 @@
 package ru.alexandr;
 
+/**
+ * Calculate.
+ *
+ * @autor Alexandr Golubev (deed27@narod.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
     public static void main(String[] args){
 	System.out.println("Hello world!");
     }
