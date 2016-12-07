659c6579f30491f993f6b2e126f0d124348d0f1c
Alexandr Golubev <deed27@narod.ru>
2016-12-07 12:41:44 +0400

    add Max.java

---
+++ chapter_001/lesson_03/src/main/java/ru/alexandr/Max.java
@@ -0,0 +1,28 @@
+package ru.alexandr;
+	/**
+	* Point.
+	* @author Alexander Golubev.
+	* @since 30.11.2016
+	* @version 1.0
+	*/
+public class Max {
+	/**
+	* поле хранит переменную max.
+	*/
+	private int max;
+	/**
+	* Метод вычисляет максимум.
+	* @param first - min namber.
+	* @param second - max namber.
+	*/
+	public void max(int first, int second) {
+		this.max = first < second ? min : max;
+	}
+	/**
+	* Метод возвращает результат.
+	* @return возвращает результат.
+	*/
+	public int getMax() {
+		return this.result;
+	}
+}
