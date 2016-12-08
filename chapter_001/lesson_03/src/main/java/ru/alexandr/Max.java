9fb1abcc331e0ced88e02098d99a2e70ca15030a
Alexandr Golubev <deed27@narod.ru>
2016-12-08 18:55:33 +0400

    add Max.java

--- chapter_001/lesson_03/src/main/java/ru/alexandr/Max.java
+++ chapter_001/lesson_03/src/main/java/ru/alexandr/Max.java
@@ -7,16 +7,12 @@ package ru.alexandr;
 	*/
 public class Max {
 	/**
-	* поле хранит переменную max.
-	*/
-	private int max;
-	/**
 	* Метод вычисляет максимум.
 	* @param first - первая переменная.
 	* @param second - вторая переменная.
 	* @return return - возвращает значение.
 	*/
 	public int max(int first, int second) {
-		return first > second ? first : second;
+		return first < second ? first : second;
 	}
 }
