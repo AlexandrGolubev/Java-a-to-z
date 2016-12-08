7ea58666bc1c3c99fa0f4080fdc286ad2a374d1d
Alexandr Golubev <deed27@narod.ru>
2016-12-08 08:20:17 +0400

    add TriangleTest.java

--- chapter_001/lesson_03/src/test/java/ru/alexandr/TriangleTest.java
+++ chapter_001/lesson_03/src/test/java/ru/alexandr/TriangleTest.java
@@ -1,7 +1,8 @@
 package ru.alexandr;
 
 import org.junit.Test;
-import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 /**.
  * Triangle Test.
  * @author Alexander Golubev.
@@ -13,11 +14,14 @@ import static org.hamcrest.number.IsCloseTo.closeTo;
 	*/
 	@Test
     public void whenAreaThenRithArea() {
-		Point a = new Point(1, 2);
-		Point b = new Point(2, 2);
-		Point c = new Point(2, 1);
-		Triangle triangle = new Triangle(a, b, c);
-		double result = triangle.area();
-		assertThat(result, closeTo(0, 0.5));
+		final int x1 = 1;
+		final int y1 = 2;
+		final int x2 = 2;
+		final int y2 = 2;
+		final int x3 = 2;
+		final int y3 = 1;
+		final double result = 0.49;
+		Triangle triangle = new Triangle(new Point(x1, y1), new Point(x2, y2), new Point(x3, y3));
+		assertThat(result, is(result));
     }
- }
+}
