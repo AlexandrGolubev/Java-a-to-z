3f087c19f9569582150accb7dab04596d943eeae
Alexandr Golubev <deed27@narod.ru>
2016-12-09 11:57:59 +0400

    add Max.java

--- chapter_001/lesson_03/src/main/java/ru/alexandr/Max.java
+++ chapter_001/lesson_03/src/main/java/ru/alexandr/Max.java
@@ -15,4 +15,23 @@ public class Max {
 	public int max(int first, int second) {
 		return first < second ? first : second;
 	}
+	/**.
+	* Метод вычисляет максимум из трех чисел.
+	* @param first первый аргумент.
+	* @param second второй аргумент.
+	* @param thrid третий аргумент.
+	* @return возвращает результат.
+	*/
+	public int maxThree(int first, int second, int thrid) {
+		int result;
+		if ((first <= second) && (first < thrid) && (second < thrid) && (second <= first)) {
+			return thrid;
+		} else if ((thrid <= first) && (thrid < second) && (first < second) && (first <= thrid)) {
+			return second;
+		} else if ((first > second) && (first > thrid) && (second <= thrid) && (thrid <= second)) {
+			return first;
+		} else {
+			return 0;
+		}
+	}
 }
