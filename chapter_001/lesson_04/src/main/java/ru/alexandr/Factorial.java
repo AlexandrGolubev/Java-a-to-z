8dd61bac5121b13e76fffda45046222ce8e49309
Alexandr Golubev <deed27@narod.ru>
2016-12-14 11:07:53 +0400

    add Factorial.java

--- chapter_001/lesson_04/src/main/java/ru/alexandr/Factorial.java
+++ chapter_001/lesson_04/src/main/java/ru/alexandr/Factorial.java
@@ -7,16 +7,13 @@ package ru.alexandr;
 	*/
 public class Factorial {
 	/**
-	* Поле хранит переменную result.
-	*/
-	private int result;
-	/**
 	* Метод вычисляет факториал.
 	* @param first - первый агрумент.
 	* @param second - второй аргумент.
 	* @return return - возвращает значение.
 	*/
 	public int factorial(int first, int second) {
+		int result = 0;
 		for (first = 1; first < second; first++) {
 			result = first * first;
 		}
